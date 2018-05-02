19e977498b6d3b5714a8c99f31555ea1de6fea91
Kobayashi Ryo <kobayashiryou@kobayashi-no-macbook-air.local>
2018-05-02 19:40:16 +0900

    Initial Commit

---
+++ オセロ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  オセロ
+//
+//  Created by Kobayashi Ryo on 2018/05/02.
+//  Copyright © 2018年 Kobayashi Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
